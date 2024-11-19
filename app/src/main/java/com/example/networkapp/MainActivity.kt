61857ef7d79eda2f165b2515c9aca5de9c268927
eeng20 <tul40997@temple.edu>
2024-11-19 15:11:04 -0500

    answered all three TO-DOs

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.networkapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -14,8 +15,12 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileOutputStream
+import java.io.FileReader
+import java.io.IOException
 
-// TODO (1: Fix any bugs)
 // TODO (2: Add function saveComic(...) to save comic info when downloaded
 // TODO (3: Automatically load previously saved comic when app starts)
 
@@ -27,10 +32,16 @@ class MainActivity : AppCompatActivity() {
     lateinit var numberEditText: EditText
     lateinit var showButton: Button
     lateinit var comicImageView: ImageView
+    private val internalFilename = "my_file.json"
+    private lateinit var file: File
 
+    // modify this for step 3 -- check for the existence of a cache file and load it if it exists
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        // Create file reference for app-specific file
+        file = File(filesDir, internalFilename)
 
         requestQueue = Volley.newRequestQueue(this)
 
@@ -43,18 +54,23 @@ class MainActivity : AppCompatActivity() {
         showButton.setOnClickListener {
             downloadComic(numberEditText.text.toString())
         }
-
+        if (file.exists()) {
+            showComic(JSONObject(file.readText()))
+        }
     }
 
     // Fetches comic from web as JSONObject
     private fun downloadComic (comicId: String) {
+
         val url = "https://xkcd.com/$comicId/info.0.json"
         requestQueue.add (
             JsonObjectRequest(url
                 , {showComic(it)}
                 , {}
             )
+
         )
+
     }
 
     // Display a comic for a given comic JSON object
@@ -62,11 +78,18 @@ class MainActivity : AppCompatActivity() {
         titleTextView.text = comicObject.getString("title")
         descriptionTextView.text = comicObject.getString("alt")
         Picasso.get().load(comicObject.getString("img")).into(comicImageView)
+        saveComic(comicObject)
     }
 
-    // Implement this function
+    // Implement this function, saves comic's info to a file
     private fun saveComic(comicObject: JSONObject) {
-
+        try {
+            file.writeText(comicObject.toString())
+            Log.d("Saved", "saved into file")
+    } catch (e: Exception) {
+        e.printStackTrace()
+            Log.d("Saved", "not saved")
+    }
     }
 
 
